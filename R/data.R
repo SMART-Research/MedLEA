6b3f643a5c33c8141e92288909cc98946380ccc1
thiyangt <thiyanga.talagala@monash.edu>
2021-03-12 05:48:02 +0530

    update documentations

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Features of Medicinal Plants 
+#'@description feature description of medicinal plant
+#'\describe{
+#'\item{Cvariable_name}{variable description}
+#'\item{plant}{plant name}
+#'\item{plant}{plant name}
+#'\item{plant}{plant name}
+#'\item{plant}{plant name}
+#'\item{plant}{plant name}
+#'\item{plant}{plant name}
+#'\item{plant}{plant name}}
+#'@source <add the url>
+#'@example
+#'data(leave this for Thiyanga)
+"leave this for Thiyanga"
