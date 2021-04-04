1bc882a26657e42a640778843ee56c9b041c35e6
Jayani Lakshika <62740181+JayaniLakshika@users.noreply.github.com>
2021-04-04 20:20:35 +0530

    Add codes to extract images

---
+++ R/Image_extract.R
@@ -0,0 +1,10 @@
+#####Function
+load_images <- function(){
+  print("The repository of leaf images of medicinal plants in Sri Lanka is collected by following the image acquisition steps that we identified.")
+  print("The repository contains 1099 leaf images of 31 species and 29-45 images per species.These have simple arrangement. The photographs were taken from the device, Huawei nova 3i. The closest photographs were captured on a white background.")
+  print("All the leaf images are in a google drive folder that anyone can access. You can download the images directly from the drive.")
+  link <- "https://drive.google.com/drive/folders/1W3ap8UhBCIVN5U_UUVSZeTh7VG4Jqbev?usp=sharing"
+  print(paste0("The shareable link: ",link))
+}
+
+load_images()
